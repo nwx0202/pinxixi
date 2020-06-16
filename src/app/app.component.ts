6d765fd1a827c60452e511f23a7b919626f18c82
nienie <765891612@qq.com>
2020-06-16 23:21:02 +0800

    pinxixi init

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+
+interface TopMenu {
+  title: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '拼夕夕';
+  menus: TopMenu[] = [{
+    title: '热门',
+    link: ''
+  }, {
+    title: '男装',
+    link: ''
+  }, {
+    title: '百货',
+    link: ''
+  }, {
+    title: '运动',
+    link: ''
+  }, {
+    title: '手机',
+    link: ''
+  }, {
+    title: '家纺',
+    link: ''
+  }, {
+    title: '食品',
+    link: ''
+  }, {
+    title: '电器',
+    link: ''
+  }, {
+    title: '鞋包',
+    link: ''
+  }, {
+    title: '汽车',
+    link: ''
+  }, {
+    title: '水果',
+    link: ''
+  }, {
+    title: '电脑',
+    link: ''
+  }, {
+    title: '内衣',
+    link: ''
+  }, {
+    title: '家装',
+    link: ''
+  }, {
+    title: '母婴',
+    link: ''
+  }, {
+    title: '美妆',
+    link: ''
+  }, {
+    title: '家具',
+    link: ''
+  }];
+}
