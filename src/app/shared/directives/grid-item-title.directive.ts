28760524fd3495fea3911bbde27586dc563a7d25
nienie <765891612@qq.com>
2020-06-21 23:43:50 +0800

    投影组件

--- src/app/shared/directives/grid-item-title.directive.ts
+++ src/app/shared/directives/grid-item-title.directive.ts
@@ -4,7 +4,9 @@ import { Directive, ElementRef, Renderer2, Input, OnInit, HostBinding } from '@a
   selector: '[appGridItemTitle]',
 })
 export class GridItemTitleDirective implements OnInit {
+  @HostBinding('style.font-size') @Input() appGridItemTitle = '0.5rem';
   @HostBinding('style.grid-area') area = 'title';
+  @HostBinding('style.white-space') wrap = 'nowrap';
   constructor(
     private elr: ElementRef,
     private rd2: Renderer2
